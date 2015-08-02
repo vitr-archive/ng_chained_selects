5fe41c39d64057221ed78b5b6fc146c1918ce5da
vitr <admin@vitr.biz>
2015-08-02 15:07:01 +1000

    google app engine finally

--- google-app-engine/option.php
+++ google-app-engine/option.php
@@ -5,9 +5,7 @@
 
 header("Access-Control-Allow-Origin: *");
 
-echo "started!!!!";
-
-$allOptions = json_decode(file_get_contents('http://ng-chained-selects.appspot.com/options.json'), true);
+$allOptions = json_decode(file_get_contents('http://ng-chained-selects.appspot.com/data/options.json'), true);
 
 $result = array();
 // option.php?mode=tree&id=20
